753b4866923b0f729baa7cf578584786168fbeb6
Denise Chan <waeyee.chan@gmail.com>
2022-05-30 15:15:07 +0100

    Initial push yay '

---
+++ den_package/lib.py
@@ -0,0 +1,4 @@
+# Try me code fun fun fun
+
+def try_me():
+    return "Don't knock it until you've tried it"
---
+++ tests/test_try_me.py
@@ -0,0 +1,6 @@
+# Test try me code even more fun
+
+from den_package.lib import try_me
+
+def test_try_me():
+    assert try_me() == "Don't knock it until you've tried it"
